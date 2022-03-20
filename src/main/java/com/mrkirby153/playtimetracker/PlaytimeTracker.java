817a5db282e83de1af934ed773fd8259dfaa9fdc
mrkirby153 <mr.austinwhyte@gmail.com>
2022-03-19 19:57:12 -0700

    Add mod boilerplate

---
+++ src/main/java/com/mrkirby153/playtimetracker/PlaytimeTracker.java
@@ -0,0 +1,23 @@
+package com.mrkirby153.playtimetracker;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("playtimetracker")
+public class PlaytimeTracker {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public PlaytimeTracker() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+        LOGGER.info("Server is starting!");
+    }
+}
