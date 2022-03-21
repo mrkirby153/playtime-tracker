27aa44356c365abd1c7aa6cc33060e4b1c3eadb5
mrkirby153 <mr.austinwhyte@gmail.com>
2022-03-20 19:29:49 -0700

    Show up as a server-side mod only

--- src/main/java/com/mrkirby153/playtimetracker/PlaytimeTracker.java
+++ src/main/java/com/mrkirby153/playtimetracker/PlaytimeTracker.java
@@ -8,8 +8,12 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedOutEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -20,6 +24,8 @@ public class PlaytimeTracker {
 
     public PlaytimeTracker() {
         MinecraftForge.EVENT_BUS.register(this);
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST,
+            () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (s, b) -> true));
     }
 
     @SubscribeEvent
